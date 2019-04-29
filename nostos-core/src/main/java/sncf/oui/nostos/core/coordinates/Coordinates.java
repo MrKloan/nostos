2ee4119e3cb946d92f11a07007231b5734b97a39
Valentin Fries <contact@fries.io>
2019-04-29 14:05:52 +0200

    Immutable Coordinates + ignored jqwik database file

--- nostos-core/src/main/java/sncf/oui/nostos/core/coordinates/Coordinates.java
+++ nostos-core/src/main/java/sncf/oui/nostos/core/coordinates/Coordinates.java
@@ -1,24 +1,26 @@
 package sncf.oui.nostos.core.coordinates;
 
-public class Coordinates {
+import org.immutables.value.Value.Immutable;
+import org.immutables.value.Value.Parameter;
 
-    private final Latitude latitude;
-    private final Longitude longitude;
-
-    private Coordinates(final Latitude latitude, final Longitude longitude) {
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
+@Immutable
+abstract class Coordinates {
 
     public static Coordinates of(final double latitude, final double longitude) {
-        return new Coordinates(Latitude.of(latitude), Longitude.of(longitude));
+        return ImmutableCoordinates.of(Latitude.of(latitude), Longitude.of(longitude));
     }
 
+    @Parameter
+    abstract Latitude latitude();
+
+    @Parameter
+    abstract Longitude longitude();
+
     public double getLatitude() {
-        return latitude.degrees();
+        return latitude().degrees();
     }
 
     public double getLongitude() {
-        return longitude.degrees();
+        return longitude().degrees();
     }
 }
