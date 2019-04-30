6e1f6b0b49e425832f0ea0bc119523df0d2e3228
Valentin Fries <contact@fries.io>
2019-04-30 17:15:17 +0200

    Longitude should throw when the degrees are out of range

--- nostos-core/src/main/kotlin/io/fries/nostos/core/coordinates/Longitude.kt
+++ nostos-core/src/main/kotlin/io/fries/nostos/core/coordinates/Longitude.kt
@@ -1,3 +1,13 @@
 package io.fries.nostos.core.coordinates
 
-class Longitude(val degrees: Double)
+class Longitude(val degrees: Double) {
+
+    companion object {
+        private const val MIN_DEGREES = -180.0
+        private const val MAX_DEGREES = 180.0
+    }
+
+    init {
+        require(degrees in MIN_DEGREES..MAX_DEGREES) { "Longitude degrees should be in the range [$MIN_DEGREES, $MAX_DEGREES]" }
+    }
+}
--- nostos-core/src/test/kotlin/io/fries/nostos/core/coordinates/LongitudeTest.kt
+++ nostos-core/src/test/kotlin/io/fries/nostos/core/coordinates/LongitudeTest.kt
@@ -2,6 +2,7 @@ package io.fries.nostos.core.coordinates
 
 import net.jqwik.api.*
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 
 class LongitudeTest {
 
@@ -21,5 +22,21 @@ class LongitudeTest {
     @Provide
     fun valid_degrees(): Arbitrary<Double> {
         return Arbitraries.doubles().between(MIN_DEGREES, MAX_DEGREES).unique()
+    }
+
+    @Property
+    internal fun should_throw_when_the_degrees_are_out_of_range(@ForAll("out_of_range_degrees") degrees: Double) {
+        assertThatIllegalArgumentException()
+                .isThrownBy { Longitude(degrees) }
+                .withMessage("Longitude degrees should be in the range [$MIN_DEGREES, $MAX_DEGREES]")
+                .withNoCause()
+    }
+
+    @Provide
+    fun out_of_range_degrees(): Arbitrary<Double> {
+        val lowerDegrees = Arbitraries.doubles().lessOrEqual(MIN_DEGREES).filter { degrees -> degrees != MIN_DEGREES }.unique()
+        val upperDegrees = Arbitraries.doubles().greaterOrEqual(MAX_DEGREES).filter { degrees -> degrees != MAX_DEGREES }.unique()
+
+        return Arbitraries.oneOf(lowerDegrees, upperDegrees)
     }
 }
