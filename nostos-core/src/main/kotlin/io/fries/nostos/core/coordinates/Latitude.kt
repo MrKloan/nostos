5cc71a119d8b4eb1117611187830888e262439a1
Valentin Fries <contact@fries.io>
2019-04-30 16:56:43 +0200

    Latitude should throw when the degrees are out of bounds

--- nostos-core/src/main/kotlin/io/fries/nostos/core/coordinates/Latitude.kt
+++ nostos-core/src/main/kotlin/io/fries/nostos/core/coordinates/Latitude.kt
@@ -1,3 +1,13 @@
 package io.fries.nostos.core.coordinates
 
-class Latitude(val degrees: Double)
+class Latitude(val degrees: Double) {
+
+    companion object {
+        private const val MIN_DEGREES = -90.0
+        private const val MAX_DEGREES = 90.0
+    }
+
+    init {
+        require(degrees in MIN_DEGREES..MAX_DEGREES) { "Latitude degrees should be in the range [$MIN_DEGREES, $MAX_DEGREES]" }
+    }
+}
--- nostos-core/src/test/kotlin/io/fries/nostos/core/coordinates/LatitudeTest.kt
+++ nostos-core/src/test/kotlin/io/fries/nostos/core/coordinates/LatitudeTest.kt
@@ -2,6 +2,7 @@ package io.fries.nostos.core.coordinates
 
 import net.jqwik.api.*
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 
 class LatitudeTest {
 
@@ -21,5 +22,21 @@ class LatitudeTest {
     @Provide
     fun valid_degrees(): Arbitrary<Double> {
         return Arbitraries.doubles().between(MIN_DEGREES, MAX_DEGREES).unique()
+    }
+
+    @Property
+    internal fun should_throw_when_the_degrees_are_out_of_bounds(@ForAll("out_of_bounds_degrees") degrees: Double) {
+        assertThatIllegalArgumentException()
+                .isThrownBy { Latitude(degrees) }
+                .withMessage("Latitude degrees should be in the range [$MIN_DEGREES, $MAX_DEGREES]")
+                .withNoCause()
+    }
+
+    @Provide
+    fun out_of_bounds_degrees(): Arbitrary<Double> {
+        val lowerDegrees = Arbitraries.doubles().lessOrEqual(MIN_DEGREES).filter { degrees -> degrees != MIN_DEGREES }.unique()
+        val upperDegrees = Arbitraries.doubles().greaterOrEqual(MAX_DEGREES).filter { degrees -> degrees != MAX_DEGREES }.unique()
+
+        return Arbitraries.oneOf(lowerDegrees, upperDegrees)
     }
 }
